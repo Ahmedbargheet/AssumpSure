995640ac19aa4dfa888872e8ea3776617eabf186
Ahmed Bargheet <ahmed.bargheet@yahoo.com>
2025-07-07 18:07:16 +0200

    new

--- R/launch_app.R
+++ R/launch_app.R
@@ -2,6 +2,11 @@
 #'
 #' @return Runs the Shiny app.
 #' @export
+#'
+#' @examples
+#' if (interactive()) {
+#'   launch_app()
+#' }
 launch_app <- function() {
   app_dir <- system.file("app", package = "AssumpSure")
   if (app_dir == "") stop("Could not find app directory. Try re-installing the package.", call. = FALSE)
